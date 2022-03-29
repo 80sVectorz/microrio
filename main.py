6933c9d09ca212daa829be4f93bc5e1fc977fffd
80sVectorz <66908776+krijnlol@users.noreply.github.com>
2022-03-29 12:42:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def on_button_pressed_a():
         elif world[4-plr_pos[1]][clamp(plr_pos[0] - 1, 0, len(world[0]))] == 0:
             plr_pos = [clamp(plr_pos[0] - 1, 0, len(world[0])), plr_pos[1]]
     else:
-        if world[5-(plr_pos[1]+2)][plr_pos[0]+1-view_pos[0]] == 0:
-            r_pos = [clamp(plr_pos[0] + 1, 0, len(world[0])), clamp(plr_pos[1] + 2, 0, len(world))]
+        if world[4-(plr_pos[1]+2)][plr_pos[0]+1-view_pos[0]] == 0:
+            plr_pos = [clamp(plr_pos[0] + 1, 0, len(world[0])), clamp(plr_pos[1] + 2, 0, len(world))]
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def draw():
@@ -22,7 +22,8 @@ def draw():
                 led.plot_brightness(x, y, 255)
             else:
                 led.plot_brightness(x, y, 0)
-            led.plot_brightness(plr_pos[0]-view_pos[0], 4-plr_pos[1], t%2*100+50)
+            if 4-plr_pos[1] < 5:
+                led.plot_brightness(plr_pos[0]-view_pos[0], 4-plr_pos[1], t%2*100+50)
 
 def on_button_pressed_b():
     global plr_pos,view_pos,r_pressed,l_pressed,world
@@ -32,7 +33,7 @@ def on_button_pressed_b():
         elif world[4-plr_pos[1]][plr_pos[0]-view_pos[0]+1] == 0:
             plr_pos = [clamp(plr_pos[0] + 1, 0, len(world[0])), plr_pos[1]]
     else:
-        if world[5-(plr_pos[1]+2)][plr_pos[0]-view_pos[0]-1] == 0:
+        if world[4-(plr_pos[1]+2)][plr_pos[0]-view_pos[0]-1] == 0:
             plr_pos = [clamp(plr_pos[0] - 1, 0, len(world[0])), clamp(plr_pos[1] + 2, 0, len(world))]
 input.on_button_pressed(Button.B, on_button_pressed_b)
 level1 = [
@@ -42,35 +43,60 @@ level1 = [
     [0, 1, 1, 1, 1,0,0,0,0],
     [1, 1, 1, 1, 1,0,0,0,0]]
 
-levels = {1:level1}
-level_start_positions = {1:[0,2]}
+level2 = [
+    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0, 0, 0, 0, 0,1,1,1,0,0,0,0,0,0,0,0,0],
+    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,1,0],
+    [0, 1, 1, 1, 1,0,0,0,0,0,1,1,1,0,0,0,0],
+    [1, 1, 1, 1, 1,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+level3 = [
+    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0, 0, 0, 0, 0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0],
+    [0, 1, 1, 1, 1,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0],
+    [1, 1, 1, 1, 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0]]
+
+levels = [level1,level2,level3]
+level_start_positions = [[0,2],[0,2],[0,2]]
 
 
 
 t = 0
-lvl = 1
+lvl = 0
+winLvl = 3
+won = False
 world = levels[lvl]
-plr_pos = level_start_positions[1]
+plr_pos = level_start_positions[lvl]
 view_pos = [0,0]
 r_pressed = False
 l_pressed = False
 
 def reset(lvl):
+    global world,plr_pos,view_pos,t, won
     world = levels[lvl]
     plr_pos = [0, 2]
     view_pos = [0,0]
     t = 0   
 
 def on_forever():
-    global plr_pos,t,r_pressed,l_pressed
-    if t%5==0:
+    global plr_pos,t,r_pressed,l_pressed, lvl, winLvl
+    if t%4==0:
         if plr_pos[1] - 1 < 0:
+            basic.show_icon(IconNames.SKULL)
+            basic.pause(1000)
             reset(lvl)
         elif plr_pos[1]-1 < len(world)-1 and world[len(world)-plr_pos[1]][plr_pos[0]] == 0 :
             plr_pos[1]-=1
+    if plr_pos[0] >= len(world[0])-1:
+        lvl += 1
+        if lvl == winLvl:
+            won = True
+            basic.show_string("Win!")
+        else:        
+            reset(lvl) 
     draw()
-    basic.pause(200)
+    basic.pause(300)
     t+=1
-    #basic.pause(50)
 
 basic.forever(on_forever)
